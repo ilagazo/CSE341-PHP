ddfbc3a0ea3b39b34020dbec0e8655bcc02a3d5e
ilagazo <lag14002@byui.edu>
2021-02-03 20:52:09 -0700

    adding stuff for project

---
+++ web/Project01/footer.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!-- Local CSS -->
+    <link rel="stylesheet" href="project01.css">
+</head>
+
+<body>
+    <footer>
+        <p>CONTACT US:<br>Customer Services: 951-651-7889<br>E-mail: JSMR@gmail.com<br></p>
+        <p>All images were pulled from Google Images and belong to their respective owners.<br><br>Related Links:</p>
+        <div class="footer-links">
+            <a href="../Week02/homepage.php">Home</a>
+            <a href="https://www.linkedin.com/in/ivanro-lagazo/">LinkedIn</a>
+            <a href="https://github.com/ilagazo">GitHub</a>
+            <a href="../Week02/assignments.php">CSE 341 Assignments</a>
+        </div>
+    </footer>
+</body>
+
+</html>
---
+++ web/Project01/navbar.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!-- Local CSS -->
+    <link rel="stylesheet" href="project01.css">
+</head>
+
+<body>
+    <nav>
+        <a href="../Project01/services.php">Services</a>
+        <a href="../Project01/employeePortalLogin.php">Employee Portal</a>
+    </nav>
+</body>
+
+</html>
